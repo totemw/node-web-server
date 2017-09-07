8bb18f51f69b232cce4e552dc7af94479cf8faf8
totem <wyc980402@icloud.com>
2017-09-07 01:13:46 -0400

    Add project name

--- server.js
+++ server.js
@@ -27,6 +27,12 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page'
+    });
+});
+
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
